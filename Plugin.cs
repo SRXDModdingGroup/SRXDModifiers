90581826dbac6f37c3320ae66ba3e24c4c5ef363
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-03-18 17:06:21 -0700

    Add initial files

---
+++ Plugin.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using SpinCore;
+
+namespace SRXDModifiers;
+
+[BepInDependency("com.pink.spinrhythm.moddingutils", "1.0.2")]
+[BepInDependency("com.pink.spinrhythm.spincore")]
+[BepInPlugin("SRXD.Modifiers", "Modifiers", "1.0.0.0")]
+public class Plugin : SpinPlugin {
+    
+}
